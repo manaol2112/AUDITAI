39abcfe124798d28a6817e576cd3bd4115b1bac8
manaol2112 <manaol2112@gmail.com>
2024-11-07 10:24:52 -0800

    update authservice

--- react/src/services/AuthService.js
+++ react/src/services/AuthService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:8000/api'; // Replace with your Django backend URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 const AuthService = {
 
